29391adac865eb046925d48d4a862e856041087f
catshirt <caden@catshirt.net>
2013-02-23 01:22:18 -0500

    more things

--- bin/main.js
+++ bin/main.js
@@ -11,12 +11,13 @@ require([
   nconf.defaults({
 
     http: {
-      port: 80
+      port: 8000
     },
 
     paths: {
       src: process.cwd(),
-      resources: null
+      js: 'js',
+      resources: 'js/resources'
     },
 
     db: {
@@ -31,13 +32,22 @@ require([
     db_conf = nconf.get('db'),
     http_conf = nconf.get('http'),
     src = path_conf.src,
+    js = path_conf.js,
     resources = path_conf.resources,
     port = http_conf.port;
 
   function serve(resources) {
     var app = brainy(src, db_conf, resources);
     app.listen(port);
-  }; 
+  };
+
+  // assume all modules we load from now on
+  // are in project-land (this allows resources to resolve dependencies
+  // relative to the project src) ... (removing the /#{js} will allow loading 
+  // relative to the file, but not relative to the src dir.. dunno how to get both)
+  require.config({
+    baseUrl: src + '/' + js
+  });
 
   if (resources) {
     file.walk(resources, function(n, p, d, files) {
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 define([
+  'http',
+  'socket.io',
   'express',
   'backbone',
   'brainy-sync',
   'brainy-api',
+  'brainy-subscribe',
+  'brainy-subscribe-api',
   'module',
   'path'
-], function(express, Backbone, Sync, Api, module, path) {
+], function(http, socketio, express, Backbone, Sync, Api, Subscribe, SubscribeApi, module, path) {
 
   return function(src_dir, db_conf, resources) {
     var app = express(),
+      server = http.createServer(app),
+      io = socketio.listen(server),
       filename = module.uri,
       dirname = path.dirname(filename),
       node_modules = dirname + '/../node_modules';
@@ -23,7 +29,15 @@ define([
     // create REST endpoints
     Api(app, resources);
 
-    return app;
+    // MongoDB subscription data
+    Backbone.subscribe = Subscribe(db_conf);
+    Backbone.Model.prototype.subscribe = Backbone.subscribe;
+    Backbone.Collection.prototype.subscribe = Backbone.subscribe;
+
+    // create WS endpoints
+    SubscribeApi(io, resources);
+
+    return server;
   };
 
 });
