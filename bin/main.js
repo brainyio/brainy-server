747d026cd1d46250d8fa9723369fd2491d034d52
catshirt <caden@catshirt.net>
2013-02-27 04:06:35 -0500

    putting rest/static on different ports

--- bin/main.js
+++ bin/main.js
@@ -10,53 +10,46 @@ require([
 
   nconf.defaults({
 
-    http: {
+    'static': {
+      src: process.cwd(),
+      js: 'js',
       port: 8000
     },
 
-    paths: {
-      src: process.cwd(),
-      js: 'js',
+    'rest': {
+      port: 8080,
       resources: 'js/resources'
     },
 
-    db: {
-      host: '127.0.0.1',
-      port: 27017,
-      name: 'brainy-server'
+    'sync': {
+      adapter: 'mongodb',
+      options: {
+        host: '127.0.0.1',
+        port: 27017,
+        name: 'brainy-server'
+      }
     }
 
   });
 
-  var path_conf = nconf.get('paths'),
-    db_conf = nconf.get('db'),
-    http_conf = nconf.get('http'),
-    src = path_conf.src,
-    js = path_conf.js,
-    resources = path_conf.resources,
-    port = http_conf.port;
-
-  function serve(resources) {
-    var app = brainy(src, db_conf, resources);
-    app.listen(port);
-  };
+  var static_conf = nconf.get('static'),
+    sync_conf = nconf.get('sync'),
+    rest_conf = nconf.get('rest'),
+    http_conf = nconf.get('http');
 
   // assume all modules we load from now on
   // are in project-land (this allows resources to resolve dependencies
   // relative to the project src) ... (removing the /#{js} will allow loading 
   // relative to the file, but not relative to the src dir.. dunno how to get both)
   require.config({
-    baseUrl: src + '/' + js
+    baseUrl: static_conf.src + '/' + static_conf.js
   });
 
-  if (resources) {
-    file.walk(resources, function(n, p, d, files) {
-      require(files, function() {
-        serve(_.values(arguments));
-      });
+  // load all resources and start brainy
+  file.walk(rest_conf.resources, function(n, p, d, files) {
+    require(files, function() {
+      brainy(static_conf, rest_conf, sync_conf, _.values(arguments));
     });
-  } else {
-    serve();
-  }
+  });
 
 });
--- src/index.js
+++ src/index.js
@@ -1,42 +1,41 @@
 define([
   'http',
-  'socket.io',
   'express',
   'backbone',
   'brainy-sync',
   'brainy-sync-api',
+  //'socket.io',
   //'brainy-subscribe',
-  //'brainy-subscribe-api',
-  'module',
-  'path'
-], function(http, socketio, express, Backbone, Sync, Api, /*Subscribe, SubscribeApi,*/ module, path) {
+  //'brainy-subscribe-api'
+], function(http, express, Backbone, Sync, SyncApi/*, socketio, Subscribe, SubscribeApi,*/) {
 
-  return function(src_dir, db_conf, resources) {
-    var app = express(),
-      server = http.createServer(app),
-      io = socketio.listen(server),
-      filename = module.uri,
-      dirname = path.dirname(filename),
-      node_modules = dirname + '/../node_modules';
+  return function(static_conf, sync_api_conf, sync_conf, resources) {
 
-    // scaffold static application
-    app.use(express.static(src_dir));
+    // create static file server
+    var static_app = express(),
+      static_server = http.createServer(static_app);
 
-    // MongoDB Backbone persistence
-    Backbone.sync = Sync(db_conf);
+    static_app.use(express.static(static_conf.src));
+    static_server.listen(static_conf.port);
+    console.log('started static server at port ' + static_conf.port + '...');
 
-    // create REST endpoints
-    Api(app, resources);
+    // create REST api
+    var rest_app = express(),
+      rest_server = http.createServer(rest_app);
 
-    // MongoDB subscription data
-    //Backbone.subscribe = Subscribe(db_conf);
+    Backbone.sync = Sync(sync_conf.adapter, sync_conf.options);
+    SyncApi(rest_app, resources);
+    rest_server.listen(sync_api_conf.port);
+    console.log('started rest server at port ' + sync_api_conf.port + '...');
+
+    // server WS api
+    // var io = socketio.listen(static_server);
+    //Backbone.subscribe = Subscribe(sync);
     //Backbone.Model.prototype.subscribe = Backbone.subscribe;
     //Backbone.Collection.prototype.subscribe = Backbone.subscribe;
-
-    // create WS endpoints
     //SubscribeApi(io, resources);
 
-    return server;
+    return static_server;
   };
 
 });
